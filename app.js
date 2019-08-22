ba78d0cdee4530e34c16bb02c2cefea53c09f8ee
Spearlpandi <muthu@iadaptime.com>
2019-08-22 10:17:32 +0530

    allow origin tes

--- app.js
+++ app.js
@@ -8,7 +8,24 @@ const details = require("./details.json");
 const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.listen(4001, () => {
   console.log("The server started on port 3000 !!!!!!");
 });
